a21cf8a056e5f3bc6bf8a7e3472e476bd37df633
rubioreyesa <115561804+rubioreyesa@users.noreply.github.com>
2023-11-13 13:44:33 -0600

    Changes
    I implemented part 1 of the threading excersice

--- ThreadingApp.java
+++ ThreadingApp.java
@@ -1,7 +1,29 @@
+import com.sun.tools.javac.Main;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ThreadingApp {
     private List<Integer> theList = new ArrayList<>();
-    private
+    public void addElements(int num){
+        for (int i = 0; i< num; i++){
+            theList.add(i);
+            System.out.println(this.theList.size());
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadingApp threadingApp = new ThreadingApp();
+        Thread thread = new Thread(()->{threadingApp.addElements(10_000);});
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e){
+            System.out.println("Something went wrong.");
+        }
+    }
+    public ThreadingApp(){
+        Thread thread2 = new Thread(()->{this.addElements(10_000);});
+        thread2.start();
+    }
 }
